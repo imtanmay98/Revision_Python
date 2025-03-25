66fd9509d45bdf11e89bb2019f10e09fdb994f1f
imtanmay98 <imrout25@gmail.com>
2025-03-25 23:37:19 +0530

    its just print fn

---
+++ letsbegin/how_to_print.py
@@ -0,0 +1,12 @@
+print("Hellow World") #what ever you want to print just write it in -->print("")
+print('Hi I am Tanmay')
+# String:- collections of characters in double courts or single court
+# We can use single qoute in double courts, here is an e.g.
+print("Hellow 'World'")
+# Similarly we can use double qoutes in single qoutes
+
+"""error we can find, if we use single/double qoute inside the single/double qoute we ll get
+  syntax error, bellow is an e.g.
+
+"""
+#print("Hellow "World"")
