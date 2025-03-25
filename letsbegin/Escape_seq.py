f36fed508ec1140497a1cba3dd0dd7211c586d6d
imtanmay98 <imrout25@gmail.com>
2025-03-25 23:51:44 +0530

    its just esc fn

---
+++ letsbegin/Escape_seq.py
@@ -0,0 +1,12 @@
+print("hellow \"world\"")# why? because \" = "
+"""
+similarly,
+\' = '
+\\ = \
+\n = new line
+\t = tab or space
+\b = backspace
+"""
+# with escape seq we can use double/single qoutes under double/single qoutes
+print("line A\nline B\tline C")
+print("this is \\ back slash")
