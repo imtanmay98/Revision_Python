0421aa566873aa5bad6037c3fcedf5eb2f21dcba
imtanmay98 <imrout25@gmail.com>
2025-03-25 23:59:06 +0530

    its just comment fn

---
+++ letsbegin/comments.py
@@ -0,0 +1,7 @@
+"""
+comments are used to give information to the user who is going to use the code
+
+"""
+print ('when line starts with # it becomes a comment, & for multiple comments we use """ """')
+
+# shortcut to comment a line is by pressing cntrl+/
