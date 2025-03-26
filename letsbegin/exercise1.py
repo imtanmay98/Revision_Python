4f615162af9576e2ffc53726a94c1777bec5902d
imtanmay98 <imrout25@gmail.com>
2025-03-27 00:04:01 +0530

    its just exercise fn

---
+++ letsbegin/exercise1.py
@@ -0,0 +1,4 @@
+print("This is \\\\ double back slash")#o/p: This is \\ double back slash
+print("These are /\\/\\/\\/\\/\\ mountains")#o/p: These are /\/\/\/\/\ mountains
+print("he is \tawesom")#o/p: he is 	awesom
+print("\\\"\t\\n\t\\t\t\\'")#o/p: \"	\n	\t	\'
