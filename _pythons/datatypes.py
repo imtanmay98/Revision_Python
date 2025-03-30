6e2d24c5fb8fd23ed6c4b2b22749f3e6d165aeea
imtanmay98 <imrout25@gmail.com>
2025-03-30 14:33:25 +0530

    my_datatypes

---
+++ _pythons/datatypes.py
@@ -0,0 +1,22 @@
+"""
+datatypes:>
+string: sentance, word, charecter
+integer: whole numbers 2,1,3,4,...
+float:decimal number 1.1,2.1,2.3,4.5.....
+boolean:true or false
+
+Object:>
+List:
+Dictionary:
+set/tuple:
+
+"""
+a = 10
+print(a)
+b = 20.4
+print(b)
+c = "something"
+print(c)
+d = True
+print(d)
+print(a>b)
