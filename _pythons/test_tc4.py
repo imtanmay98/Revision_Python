f1a67c30bba5fe8d0be4e35d029db825fa301305
imtanmay98 <imrout25@gmail.com>
2025-04-01 03:12:12 +0530

    tc4

---
+++ _pythons/test_tc4.py
@@ -0,0 +1,20 @@
+import pytest
+import allure
+import requests
+
+@allure.title("To verify GET request-> booking API")
+@allure.description("To verify it gives a sucess response.")
+@allure.tag("regression", "p0", "smoke")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.label("owner", "Tanmay Rout")
+@allure.link("https://dev.example.com/", name="Website")
+@allure.issue("AUTH-123")
+@allure.testcase("TC#4")
+
+
+@pytest.mark.smoke
+def test_single_get_req_resp():
+    url="https://restful-booker.herokuapp.com/booking"
+    response_data= requests.get(url)
+    print(response_data.headers)
+    assert response_data.status_code == 200
