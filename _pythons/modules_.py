ce5b61e6647e133d8c83bec8f4d27d27f71fe676
imtanmay98 <imrout25@gmail.com>
2025-03-31 17:20:31 +0530

    modules_basics

---
+++ _pythons/modules_.py
@@ -0,0 +1,10 @@
+# import datetime
+# current_time = datetime.datetime.now()
+# print(current_time)
+from datetime import datetime
+cur_time = datetime.now()
+print(cur_time)
+
+
+from math import sqrt
+print(int(sqrt(49)))
