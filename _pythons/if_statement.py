95e9d772ea14bc22459dbcdf737bc208bcee6555
imtanmay98 <imrout25@gmail.com>
2025-03-31 16:08:38 +0530

    my_statements with explanation

---
+++ _pythons/if_statement.py
@@ -0,0 +1,27 @@
+# Age= int(input("Please enter your age = "))
+# print("Your Age is", Age)
+#
+# User input:
+# print("Please enter your age")
+# age = int(input())
+# print("You are",age ,"years old")
+## Below is if statement, used to check wheather the partiular cond is true oe not,
+#it only executes if the condition is true else no output,
+# if Age==18:
+#     print("Yes! you are 18")
+
+# below is if-else statement, it executes the outpur regardless if it is true or false
+# if Age >= 18:
+#     print("Congratulations you are authorized to join the party!!")
+# else:
+#     print("We are really sorry!! you can not enter the party since you are a minor")
+#if-elif-else statemnt when there is multiple conditions
+marks= int(input("Please enter your marks\n"))
+if marks >= 90:
+    print("Your grade is O")
+elif marks >= 70:
+    print("Your grade is E")
+elif marks >= 50:
+    print("Your grade is A")
+else:
+    print("Your grade is B")
