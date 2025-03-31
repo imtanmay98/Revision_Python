609e95f07c1c7a17b16d90fdb35ec9ce6d7b8b08
imtanmay98 <imrout25@gmail.com>
2025-03-31 17:01:26 +0530

    for_loops with explanation

---
+++ _pythons/for_loops.py
@@ -0,0 +1,40 @@
+# list = [1,2,3,4,5,6,7,8]
+#
+# for i in list:
+#     print(i)
+
+# above was the simplest one, below we will use statements inside for loop
+# k = input("enter your cupon number\n")
+# nums = [11,23,44,33,56,78,88]
+# for k in nums:
+#     if k <=8:
+#         print("yor balance is too low")
+#     if k <=10:
+#         print("your balance is not enough")
+#     if k in nums:
+#         print("You can make a call")
+# marks = int(input("enter your marks\n"))
+# numbers = range(100)
+# for i in numbers:
+#     if marks >= 90:
+#         print("Your grade is O")
+#         break
+#     elif marks >= 70:
+#         print("Your grade is E")
+#         break
+#     elif marks >= 50:
+#         print("Your grade is A")
+#         break
+#     else:
+#         print("Your grade is B")
+#         break
+
+
+for i in range(5):
+    if i==1:
+        print("One")
+    elif i==2:
+        print("Two")
+    else:
+        print("Other")
+
